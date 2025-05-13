9354382f4f4b78d4d458e15125270c269fcb810d
devgrantjpg <devmodegrant@gmail.com>
2025-05-13 12:41:46 -0700

    eslint

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*", // Allow images from any domain
+        port: "",
+      },
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // ✅ disables ESLint at build time
+  },
 };
 
 export default nextConfig;
